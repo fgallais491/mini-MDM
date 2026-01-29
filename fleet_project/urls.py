9919c39748e8b141833bd52f2aa90507808a0745
fgallais491 <gallaisflorian01@gmail.com>
2026-01-29 13:49:53 +0100

    adding home page, auth and admin user

--- fleet_project/urls.py
+++ fleet_project/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse 
+
+def home(request):
+    return HttpResponse("The API is running add api/ or admin/ in the url to see things")
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
